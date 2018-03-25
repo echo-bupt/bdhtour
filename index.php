13e3ba6a437c67b246c319dfbe6fd99b7067456f
echo-bupt <31395549+echo-bupt@users.noreply.github.com>
2018-03-25 03:01:23 +0000

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 define("APP_NAME","index");//定义应用名
 define("DEBUG",1);
 define("COMPILE",false);
-define("APP_PATH","./index/");//注意这里要指定路径！这里的./指的就是网站根目录下!
+define("APP_PATH","./index/");
 error_reporting(0);																
  include "hdphp/hdphp/hdphp.php";
 ?>
