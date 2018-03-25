421fdb7490b319210b3b9577ccb0745beab7867e
echo-bupt <31395549+echo-bupt@users.noreply.github.com>
2018-03-25 03:01:40 +0000

    Update admin.php

--- admin.php
+++ admin.php
@@ -3,7 +3,7 @@ define("APP_NAME","admin");//定义应用名
 define("DEBUG",0);
 error_reporting(0);
 define("COMPILE",false);
-define("APP_PATH","./admin/");//注意这里要指定路径！
+define("APP_PATH","./admin/");
 include "hdphp/hdphp/hdphp.php";
 ?>
 
